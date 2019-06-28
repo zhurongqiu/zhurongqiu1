a18547be069c4a9c7c348bd4f2b16d2f045fccd8
zhurongqiu <452816645@qq.com>
2019-06-28 10:42:28 +0800

    123

---
+++ port_sort.py
@@ -0,0 +1,6 @@
+
+port_list = ['eth 1/101/1/42','eth 1/101/1/26','eth 1/101/1/23','eth 1/101/1/7','eth 1/101/2/46','eth 1/101/1/34','eth 1/101/1/18','eth 1/101/1/13','eth 1/101/1/32','eth 1/101/1/25','eth 1/101/1/45','eth 1/101/2/8']
+
+port_list.sort(key=lambda x:(int(x[4]),int(x[6:9]),int(x[10]),int(x[12:])))
+
+print(port_list)
